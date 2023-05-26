a2c8e8101b8073a9ee565f05242b8144c425151b
kblogin123456 <kb.login@gmail.com>
2023-05-26 15:59:50 +0530

    replaced require with import

--- index.js
+++ index.js
@@ -1,83 +1,94 @@
-const application = process.env.INPUT_APPLICATION;
-const applicationProcess = process.env.INPUT_APPLICATIONPROCESS;
-const environment = process.env.INPUT_ENVIRONMENT;
-const onlyChanged = process.env.INPUT_ONLYCHANGED === 'true';
-const versions = process.env.INPUT_VERSIONS.split(',');
-const hostname = process.env.INPUT_HOSTNAME;
-const port = process.env.INPUT_PORT;
-const username = process.env.INPUT_USERNAME;
-const password = process.env.INPUT_PASSWORD;
-const component = process.env.INPUT_COMPONENT;
+// const application = process.env.INPUT_APPLICATION;
+// const applicationProcess = process.env.INPUT_APPLICATIONPROCESS;
+// const environment = process.env.INPUT_ENVIRONMENT;
+// const onlyChanged = process.env.INPUT_ONLYCHANGED === 'true';
+// const versions = process.env.INPUT_VERSIONS.split(',');
+// const hostname = process.env.INPUT_HOSTNAME;
+// const port = process.env.INPUT_PORT;
+// const username = process.env.INPUT_USERNAME;
+// const password = process.env.INPUT_PASSWORD;
+// const component = process.env.INPUT_COMPONENT;
 
-// const hostname = "10.134.119.177"
-// const port = "8443"
-// const username = 'admin';
-// const password = 'admin';
+const hostname = "10.134.119.177"
+const port = "8443"
+const username = 'admin';
+const password = 'admin';
+const application = 'MYAPP';
+const applicationProcess = 'DEPLOY-HFS ';
+const environment = 'DEV';
+const onlyChanged = false;
+const versions = 'hfs_only_rabo_uid'.split(',');
+const component = 'MYCOMP';
 let requestId='';
 let intervalId; 
-  import('node-fetch')
-  .then((module) => {
-    const fetch = module.default;
-    const https = require('https');    
-    const apiUrl = 'https://'+hostname+':'+port+'/cli/applicationProcessRequest/request'; 
+const https = require('https'); 
+import('node-fetch')
+.then((module) => {
+  const fetch = module.default;   
+  const apiUrl = 'https://'+hostname+':'+port+'/cli/applicationProcessRequest/request'; 
 
-    const data = {
-      "application": application,
-      "applicationProcess": applicationProcess,
-      "environment": environment,
-      "onlyChanged": onlyChanged,
-      "versions": versions.map(version => ({
-        "version": version,
-        "component": component
-      }))
-    };
+  const data = {
+    "application": application,
+    "applicationProcess": applicationProcess,
+    "environment": environment,
+    "onlyChanged": onlyChanged,
+    "versions": versions.map(version => ({
+      "version": version,
+      "component": component
+    }))
+  };
 
 
-    const authHeader = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
+  const authHeader = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
 
-    const httpsAgent = new https.Agent({
-      rejectUnauthorized: false // Ignore SSL verification
-    });
-
-    fetch(apiUrl, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': authHeader // Include the basic authentication header
-      },
-      body: JSON.stringify(data),
-      agent: httpsAgent
-    })
-      .then(response => response.json())
-      .then(result => {
-        console.log('API response:', result);
-        requestId = result.requestId;
-        console.log('Request-ID:', requestId);
-        intervalId = setInterval(triggerAPI, 5000);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  })
-  .catch((error) => {
-    console.error('Error:', error);
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false // Ignore SSL verification
   });
 
+  fetch(apiUrl, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': authHeader // Include the basic authentication header
+    },
+    body: JSON.stringify(data),
+    agent: httpsAgent
+  })
+    .then(response => response.json())
+    .then(result => {
+      console.log('API response:', result);
+      requestId = result.requestId;
+      console.log('Request-ID:', requestId);
+      intervalId = setInterval(triggerAPI, 5000);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
 
-  function triggerAPI() {
 
-    const https = require('https');
+function triggerAPI() {
+
+  import('node-fetch')
+  .then((module) => {
     console.log(" Will poll till completion of the UCD process with Request ID :- "+requestId);
+    
+    const fetch = module.default;     
+    const authHeader = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
     const apiUrl = 'https://'+hostname+':'+port+'/cli/applicationProcessRequest/requestStatus?request='+requestId
     const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-    const fetch = require('node-fetch');
 
     fetch(apiUrl, {
-      agent: httpsAgent,
+      method: 'GET',
       headers: {
         'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`
-      }
+      },
+      agent: httpsAgent
     })
       .then(response => response.json())
       .then(result => {
@@ -90,8 +101,13 @@ let intervalId;
       })
       .catch(error => {
         console.error('Error:', error);
-        process.exit(1); // Exit the program on exception
+        process.exit(1); 
       });
-  }
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+    process.exit(1); 
+  });
+}
   
 
